46fbbd58c866896b05e65b81d562e0487bd52617
W1llb0 <106029481+W1llb0@users.noreply.github.com>
2022-06-30 13:26:41 +0300

    Add files via upload

--- ASTERY/scripts/script.js
+++ ASTERY/scripts/script.js
@@ -1,11 +1,22 @@
 const swiper = new Swiper('#swiper-image', {
     loop: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.olives__slider__button__next',
+        prevEl: '.olives__slider__button__prev'
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: '.slider__pagination',
         clickable: true,
     },
 });
+
+const swiperDiscount = new Swiper('#swiper__discount', {
+    loop: true,
+    navigation: {
+        nextEl: '.slider__discount__button__next',
+        prevEl: '.slider__discount__button__prev'
+    },
+    slidesPerView: 4,
+});
+
+
